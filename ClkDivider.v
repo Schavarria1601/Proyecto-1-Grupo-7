1c83b8eeaccb5e60ae7cf4a9599a8b5aa922c6a5
Stephanie <sm.chavarria@gmail.com>
2017-03-08 17:44:43 -0600

    Divisor de Frecuencia

---
+++ ClkDivider.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/26/2017 11:05:16 PM
+// Design Name: 
+// Module Name: ClkDivider
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+//https://learn.digilentinc.com/Documents/262
+
+module ClkDivider(
+    input clk,
+    input rst,
+    output reg clk_div
+    );
+    
+localparam constantNumber = 2; //Alguien que me explique por que esto no funciona
+
+reg [31:0] count;
+ 
+always @ (posedge(clk), posedge(rst))
+begin
+    if (rst == 1'b1)
+        count <= 32'b0;
+    else if (count == constantNumber - 1)
+        count <= 32'b0;
+    else
+        count <= count + 1;
+end
+
+always @ (posedge(clk), posedge(rst))
+begin
+     if (rst == 1'b1)
+        clk_div <= 1'b0;
+    else if (count == constantNumber - 1)
+        clk_div <= ~clk_div;
+    else
+        clk_div <= clk_div;
+end
+    
+endmodule
